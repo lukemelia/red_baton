a97970b709055274e4af9221c5c5940ffb20dd8a
Luke Melia <luke@weplay.com>
2010-07-14 22:57:30 -0400

    Spec coverage for informational headers on POST responses

--- spec/publisher_endpoint_spec.rb
+++ spec/publisher_endpoint_spec.rb
@@ -184,37 +184,79 @@ describe "publisher endpoint" do
   end
   
   context "POST requests" do
-    before(:each) do
-      start_server(RedBaton.new)
-      put('/publish/42')
-    end
-    after(:each) do
-      stop_server
+
+    describe "message delivery" do
+      before(:each) do
+        start_server(RedBaton.new)
+        put('/publish/42')
+      end
+      after(:each) do
+        stop_server
+      end
+
+      specify "The message MUST be immediately delivered to all currently long-held subscriber requests" do
+        subscriber_result_1 = subscribe('/subscribe/42')
+        subscriber_result_2 = subscribe('/subscribe/42')
+
+        post('/publish/42', 'Hi Mom')
+
+        subscriber_result_1.thread_join
+        subscriber_result_2.thread_join
+
+        subscriber_result_1.response.body.should == 'Hi Mom'
+        subscriber_result_2.response.body.should == 'Hi Mom'
+      end
+      
+      specify "the Content-Type header of the request MUST be forwarded with the message." do
+        subscriber_result = subscribe('/subscribe/42')
+        post('/publish/42', 'window.alert("Hi Mom")', {'Content-Type' => 'text/javascript'})
+        subscriber_result.thread_join
+        subscriber_result.response.header['content-type'].should == 'text/javascript'
+      end
     end
 
-    specify "The message MUST be immediately delivered to all currently long-held subscriber requests" do
-      subscriber_result_1 = subscribe('/subscribe/42')
-      subscriber_result_2 = subscribe('/subscribe/42')
+    describe "HTTP response" do
+      before(:each) do
+        start_server(RedBaton.new)
+        put('/publish/42')
+      end
+      after(:each) do
+        stop_server
+      end
+
+      it "MUST be 201 Created if there were any long-held subscribers that have been sent this message" do
+        subscriber_result = subscribe('/subscribe/42')
+        post('/publish/42', 'Hi Mom').response.code.to_i.should == 201
+        subscriber_result.thread_join
+      end
+
+      it "MUST be 202 Accepted if there were no long-held subscribers" do
+        post('/publish/42', 'Hi Mom').response.code.to_i.should == 202
+      end
+            
+      it "should include the number of subscribers in an X-Channel-Subscribers header" do
+        get('/publish/42').header['x-channel-subscribers'].should == '0'
+
+        subscriber_result = subscribe('/subscribe/42')
+
+        poll_until {
+          response = get('/publish/42').response
+          response.header['x-channel-subscribers'] == '1'
+        }
+        post('/publish/42', 'Hi Mom').header['x-channel-subscribers'].should == '1'
+        subscriber_result.thread_join
+      end
       
-      post('/publish/42', 'Hi Mom')
-      
-      subscriber_result_1.thread_join
-      subscriber_result_2.thread_join
-      
-      subscriber_result_1.response.body.should == 'Hi Mom'
-      subscriber_result_2.response.body.should == 'Hi Mom'
+      it "should include the number of messages in an X-Channel-Messages header" do
+        get('/publish/42').header['x-channel-messages'].should == '0'
+        post('/publish/42', 'Hi Mom')
+        get('/publish/42').header['x-channel-messages'].should == '1'
+        post('/publish/42', 'Hi Dad').header['x-channel-messages'].should == '2'
+      end
     end
 
-    specify "MUST be replied to with a 201 Created if there were any long-held subscribers that have been sent this message" do
-      subscriber_result = subscribe('/subscribe/42')
-      post('/publish/42', 'Hi Mom').response.code.to_i.should == 201
-      subscriber_result.thread_join
-    end
-    
-    specify "MUST be replied to with a 202 Accepted if there were no long-held subscribers" do
-      post('/publish/42', 'Hi Mom').response.code.to_i.should == 202
-    end
-    
+    xspecify "Message storage limits SHOULD be configurable. publisher locations SHOULD be configurable to allow message storage."
+
     context "message storage is configured" do
       xspecify "the message MAY be stored for future retrieval"
       xspecify "the oldest message stored for the channel MAY be deleted"
@@ -224,15 +266,6 @@ describe "publisher endpoint" do
       xspecify "messages are not stored for future retrieval"
     end
     
-    specify "the Content-Type header of the request MUST be forwarded with the message." do
-      subscriber_result = subscribe('/subscribe/42')
-      post('/publish/42', 'window.alert("Hi Mom")', {'Content-Type' => 'text/javascript'})
-      subscriber_result.thread_join
-      subscriber_result.response.header['content-type'].should == 'text/javascript'
-    end
-    
-    xspecify "Message storage limits SHOULD be configurable. publisher locations SHOULD be configurable to allow foregoing message storage on POST requests."
-    xspecify "All 200-level responses MUST, in the response body, contain information about the applicable channel. This information MAY contain the number of stored messages and the number of subscribers' requests being long-held prior to this request."
   end
   
   context "other HTTP requests" do
